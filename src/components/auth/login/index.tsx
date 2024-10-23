0746ad4d9d1df2145ce17d8ceff65e2124981c89
Emmanuel Okafor <nondefyde@gmail.com>
2024-10-23 14:56:30 +0100

    revamped login screen

--- src/components/auth/login/index.tsx
+++ src/components/auth/login/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { Button, Card, Form, Input, Row } from 'antd';
 import { useForm } from 'antd/lib/form/Form';
-import { useRouter } from 'next/navigation';
 import React from 'react';
 
 interface LoginProps {
@@ -11,7 +10,6 @@ interface LoginProps {
 
 const Login = ({ handleLogin, isLoadingLogin }: LoginProps) => {
   const [form] = useForm();
-  const { push } = useRouter();
 
   const onFinish = (values: Record<string, any>) => {
     handleLogin(values);
@@ -19,7 +17,9 @@ const Login = ({ handleLogin, isLoadingLogin }: LoginProps) => {
   return (
     <div className="w-[500px] mx-auto">
       <Card className="border shadow-md border-neutral-200 p-5">
-        <div className="font-semibold text-[22px] mb-6">Welcome back, sign in</div>
+        <h1 className="font-semibold text-[32px] mb-1">Data Migration</h1>
+        <div className="font-semibold text-[18px] mb-6">Hope you know what you want to do here</div>
+        <h3 className="font-semibold text-[22px] mb-6">Enter Password to Log In</h3>
         <Form
           form={form}
           layout="vertical"
@@ -28,16 +28,6 @@ const Login = ({ handleLogin, isLoadingLogin }: LoginProps) => {
           name="account-setting-form"
           className="mt-5 account-setting-form"
         >
-          <Row>
-            <Form.Item
-              name="email"
-              rules={[{ required: true, type: 'email', message: 'Email is invalid' }]}
-              label={<div className="mb-0 text-muted-foreground">Email</div>}
-              className="w-full"
-            >
-              <Input placeholder="Enter your email" className="w-full" size="large" />
-            </Form.Item>
-          </Row>
           <Row>
             <Form.Item
               name="password"
@@ -59,51 +49,12 @@ const Login = ({ handleLogin, isLoadingLogin }: LoginProps) => {
               disabled={false}
               block
               loading={isLoadingLogin}
-              htmlType="submit"
-              // size="large"
-            >
+              htmlType="submit">
               Continue
             </Button>
           </Row>
-          <div className="">Forgot Password</div>
         </Form>
-        <Row>
-          {/* <Button
-            className="opacity-100 mt-1.5 mb-10 text-muted-foreground h-12 font-semibold rounded-md border-neutral-200"
-            type="primary"
-            disabled={false}
-            block
-            loading={false}
-            htmlType="submit"
-            // size="large"
-          >
-            <Space className="flex items-center text-center justify-center" size={10}>
-              <Image
-                src={'/assets/svgs/google-icon.svg'}
-                alt="google-icon"
-                width={24}
-                height={24}
-              />
-              <span>Sign in with Google</span>
-            </Space>
-          </Button> */}
-          <div className="text-center mx-auto text-muted-foreground">
-            By signing up, you agree to our <br />{' '}
-            <span className="font-semibold text-blue cursor-pointer">
-              Terms of Service and Privacy Policy
-            </span>
-          </div>
-        </Row>
       </Card>
-      <div className="text-center mt-10 text-muted-foreground">
-        Don't have an account?{' '}
-        <span
-          className="font-semibold text-blue cursor-pointer"
-          onClick={() => push('/auth/sign-up')}
-        >
-          Sign up
-        </span>
-      </div>
     </div>
   );
 };
